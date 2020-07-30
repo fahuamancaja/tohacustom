cef940bd76f56d4be26b24a4b6d80b738275d92a
Nando <fahuamancaja@gmail.com>
2020-07-30 13:47:03 -0400

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -364,13 +364,14 @@ var projectCards;
           
           var wheelOpt = supportsPassive ? { passive: false } : false;
           var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-          function enableScroll() {
-            window.removeEventListener('DOMMouseScroll', preventDefault, false);
-            window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-            window.removeEventListener('touchmove', preventDefault, wheelOpt);
-            window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+          // call this to Disable
+          function disableScroll() {
+            window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+            window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+            window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+            window.addEventListener('keydown', preventDefaultForScrollKeys, false);
           }
-          enableScroll();
+          disableScroll();
         }
       }
     }
