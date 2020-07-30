d529e9ea33ed5ad7365acb9b73bb2a97b20e7d2d
Nando <fahuamancaja@gmail.com>
2020-07-30 15:15:30 -0400

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -312,38 +312,11 @@ var projectCards;
         rowNumber++;
       }
 
-      // left: 37, up: 38, right: 39, down: 40,
-      // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-      var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-      function preventDefault(e) {
-        e.preventDefault();
-      }
-
-      function preventDefaultForScrollKeys(e) {
-        if (keys[e.keyCode]) {
-          preventDefault(e);
-          return false;
-        }
-      }
-
-      // modern Chrome requires { passive: false } when adding event
-      var supportsPassive = false;
-      try {
-        window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-          get: function () { supportsPassive = true; } 
-        }));
-      } catch(e) {}
-
-      var wheelOpt = supportsPassive ? { passive: false } : false;
-      var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-
       // show full image on click
       let elements = document.getElementsByClassName("achievement-entry");
       len = elements.length;
       for (let i = 0; i < len; i++) {
-        elements[i].onclick = function first () {
+        elements[i].onclick = function () {
           let achievements = document.getElementsByClassName("achievement-entry");
           let len2 = achievements.length;
           for (let j = 0; j < len2; j++) {
@@ -363,52 +336,55 @@ var projectCards;
           }
           if (this.children["achievement-title"] != undefined) {
             this.children["achievement-title"].classList.toggle("hidden");
-          }
-          // call this to Disable
-          function disableScroll() {
-            window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-            window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-            window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-            window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-          }
-          disableScroll();
-          this.removeEventListner("click", first, false);
-          this.addEventListener("click", second, false);
-        }
-
-        function second () {
-          let achievements = document.getElementsByClassName("achievement-entry");
-          let len2 = achievements.length;
-          for (let j = 0; j < len2; j++) {
-            achievements[j].classList.toggle("hidden");
-          }
-          this.classList.toggle("achievement-details");
-          this.classList.toggle("hidden");
-          this.parentElement.classList.toggle("col-lg-12");
-          this.parentElement.classList.toggle("col-md-12");
-          this.parentElement.classList.toggle("col-sm-12");
-          if (this.children["caption"] != undefined) {
-            this.children["caption"].classList.toggle("hidden");
-          }
-          if (this.children["enlarge-icon"] != undefined) {
-            this.children["enlarge-icon"].classList.toggle("fa-search-plus");
-            this.children["enlarge-icon"].classList.toggle("fa-times");
-          }
-          if (this.children["achievement-title"] != undefined) {
-            this.children["achievement-title"].classList.toggle("hidden");
-          }
-          // call this to Enable
-          function enableScroll() {
-            window.removeEventListener('DOMMouseScroll', preventDefault, false);
-            window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-            window.removeEventListener('touchmove', preventDefault, wheelOpt);
-            window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
           }
         
-          enableScroll();
-          
+        // /////////Test
+        // // left: 37, up: 38, right: 39, down: 40,
+        // // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+        // var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+        // function preventDefault(e) {
+        //   e.preventDefault();
+        // }
+
+        // function preventDefaultForScrollKeys(e) {
+        //   if (keys[e.keyCode]) {
+        //     preventDefault(e);
+        //     return false;
+        //   }
+        // }
+
+        // // modern Chrome requires { passive: false } when adding event
+        // var supportsPassive = false;
+        // try {
+        //   window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        //     get: function () { supportsPassive = true; } 
+        //   }));
+        // } catch(e) {}
+
+        // var wheelOpt = supportsPassive ? { passive: false } : false;
+        // var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+        // // call this to Disable
+        // function disableScroll() {
+        //   window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+        //   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+        //   window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+        //   window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+        // }
+
+        // // call this to Enable
+        // function enableScroll() {
+        //   window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        //   window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+        //   window.removeEventListener('touchmove', preventDefault, wheelOpt);
+        //   window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+        // }
+
+        // /////End of Test
+        
         }
-        addEventListener("click", this.first, false);
+
       }
     
     }
